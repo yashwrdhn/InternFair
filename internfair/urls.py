394b85a6654f2cb19e51aea0d68b73a91994ee38
Ankit Saraf <ankitntse@gmail.com>
2020-10-18 18:08:02 +0530

    basic url mapping done

--- internfair/urls.py
+++ internfair/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('student/register', views.StudentRegistration, name='StudentRegistration'),
+    path('student/profile', views.StudentProfile, name='StudentProfile'),
+    path('student/availableInternships', views.AvailableInternships, name='AvailableInternships')
+
 ]
--- internfair/views.py
+++ internfair/views.py
@@ -5,6 +5,18 @@ from django.views import View
 def index(request):
     return render(request, "StudentLanding.html")
 
+def StudentRegistration(request):
+    return render(request, "StudentRegistration.html")
+
+def StudentProfile(request):
+    return render(request, "StudentProfile1.html")
+
+def AvailableInternships(request):
+    return render(request, "AvailableInternships.html")
+
+
+
+
 
 
 
