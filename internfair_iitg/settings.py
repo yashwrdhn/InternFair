f0515923a64865afc82def7dea74395a67b1c3dc
PRIYANSHU GUPTA <50921707+priyansh18a@users.noreply.github.com>
2020-12-12 21:23:37 +0530

    Update settings.py

--- internfair_iitg/settings.py
+++ internfair_iitg/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kfbcj)ilo6_ewd5n)yq^$6#g73=2v@#w!@&_s^jk47@3i3v5&&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
