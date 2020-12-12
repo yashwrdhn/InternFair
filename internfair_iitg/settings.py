da771c477ef67a64a35febe5719e71c8993ded38
PRIYANSHU GUPTA <50921707+priyansh18a@users.noreply.github.com>
2020-12-13 02:09:38 +0530

    Update settings.py

--- internfair_iitg/settings.py
+++ internfair_iitg/settings.py
@@ -139,6 +139,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "recruiter/static/recruiter"),
+#     os.path.join(BASE_DIR, "internfair/static/"),
+
+# ]
+
 LOGIN_URL = '/'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
