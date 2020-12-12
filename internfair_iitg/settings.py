52e0a995d340d95b5e565028385199c24cdb0a5b
PRIYANSHU GUPTA <50921707+priyansh18a@users.noreply.github.com>
2020-12-13 01:47:39 +0530

    Update settings.py

--- internfair_iitg/settings.py
+++ internfair_iitg/settings.py
@@ -139,11 +139,6 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "recruiter/static/recruiter"),
-    os.path.join(BASE_DIR, "internfair/static/"),
-
-]
 
 LOGIN_URL = '/'
 
